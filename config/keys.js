d11294900ec8f5ff4befa7036529902329a52d5d
Abhishek <abhisheksharma0313@gmail.com>
2020-07-19 11:40:31 +0530

    Commit 0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://DevConnector:DevConnector@cluster0.h80zp.mongodb.net/DevConnector?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(users);
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// app.get("/api/users/test", (req, res) => {
+//   res.send("Hello user");
+// });
+
+// Use Route-Handlers
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
